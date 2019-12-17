fb2e330a9fb954cea7e7e63b82951a8709cd6956
drdnar <drdnar@gmail.com>
2019-12-17 01:09:52 -0500

    Add debouncing
    I was having some trouble with one button bouncing, and I don't feel like desoldering it and replacing it, so I implemented debouncing.

--- hid_remote.ino
+++ hid_remote.ino
@@ -2,6 +2,7 @@
 
 #define ROWS 3
 #define COLUMNS 3
+#define DEBOUNCE_TIME 5
 
 // Rows, used as drivers
 const uint8_t rows[] = { 5, 3, 2 };
@@ -20,10 +21,10 @@ const uint16_t key_codes[3][3] = {
   { HID_REMOTE_REWIND, HID_REMOTE_FAST_FORWARD, HID_REMOTE_MUTE }
 };
 
-bool key_states[ROWS][COLUMNS] = {
-  {1, 1, 1},
-  {1, 1, 1},
-  {1, 1, 1},
+uint8_t key_states[ROWS][COLUMNS] = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
 };
 
 void idle_key_matrix() {
@@ -47,6 +48,10 @@ void setup() {
     pinMode(rows[i], OUTPUT);
     digitalWrite(rows[i], HIGH);
   }
+  // Need to use a pull-up or pull-down to ensure
+  // an undriven pin has a defined state.
+  // Could use external pull-down resistors,
+  // but using the internal pull-ups is less soldering.
   for (uint8_t i = 0; i < COLUMNS; i++)
     pinMode(columns[i], INPUT_PULLUP);
 }
@@ -60,14 +65,27 @@ void loop() {
     for (uint8_t j = 0; j < COLUMNS; j++)
     {
       bool key_state = digitalRead(columns[j]);
-      if (key_states[i][j] != key_state)
+      // Recall that since we're using the internal pull-ups,
+      // 0 means PRESSED and 1 means NOT pressed.
+      if (key_state)
       {
-        key_states[i][j] = key_state;
-        if (key_state)
-          HIDRemote.release(key_codes[i][j]);
-        else
+        // Not pressed, so check debouncing countdown timer
+        if (key_states[i][j])
+        {
+          key_states[i][j]--;
+          if (!key_states[i][j])
+            HIDRemote.release(key_codes[i][j]);
+        }
+      }
+      else
+      {
+        // Pressed
+        if (!key_states[i][j])
           HIDRemote.press(key_codes[i][j]);
-      } 
+        // As long as button is pressed, reset countdown timer
+        // so it doesn't decrement until button is released.
+        key_states[i][j] = DEBOUNCE_TIME;
+      }
     }
     digitalWrite(rows[i], HIGH);
   }
